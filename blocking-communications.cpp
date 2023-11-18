8fb5cbfa0699307579a450c46cbc4b4255c2caf7
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2023-11-18 16:07:51 -0300

    correção para printar somente os números

--- blocking-communications.cpp
+++ blocking-communications.cpp
@@ -10,27 +10,27 @@ int main(int argc, char** argv) {
 
   // Read the local value of the process
   // local_value will hold a specific int for process 0, and another for process 1
-  int local_value = atoi(argv[1]);
+  int local_value;
+  local_value = atoi(argv[1]);
   int other_value;
 
   if (rank == 0) {
-    std::cout << "Process #0 is executing\n";
     // Here, enter the code for the first process :
     // 1- Send the value to process 1 
     MPI_Send(&local_value, 1, MPI_INT, 1, 99, MPI_COMM_WORLD);
     // 2- Receive the value from process 1 (in other_value)
     MPI_Recv(&other_value, 1, MPI_INT, 1, 77, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     // 3- Print the sum of the two values on stdout 
-    std::cout << "Process #" << rank << " - Sum: " << local_value + other_value << std::endl;
+    std::cout << local_value + other_value << std::endl;
+    
   } else {
-    std::cout << "Process #1 is executing\n";
     // Here enter the code for the second process :
     // 1- Receive the value from process 0 (in other_value)
     MPI_Recv(&other_value, 1, MPI_INT, 0, 99, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     // 2- Send the value to process 0
     MPI_Send(&local_value, 1, MPI_INT, 0, 77, MPI_COMM_WORLD);
     // 3- Print the product of the two values on stdout
-    std::cout << "Process #" << rank << " - Product: " << local_value * other_value << std::endl;
+    std::cout << local_value * other_value << std::endl;
   }
 
   MPI_Finalize();
