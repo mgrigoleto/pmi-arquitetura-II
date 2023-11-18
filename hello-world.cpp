ce162936f2826caaa0bee97f5df5c50dde7c3413
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2023-11-18 15:29:40 -0300

    Create hello-world.cpp

---
+++ hello-world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv) {
+  // Initialization
+  MPI_Init(&argc, &argv);
+
+  // Reading size and rank
+  int size, rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+  // Printing
+  std::cout << "Hello world, from process #" << rank << std::endl;
+  
+  // Finalization
+  MPI_Finalize();
+
+  return 0;
+}
